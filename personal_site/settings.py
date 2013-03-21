0235df608e9d295e1b2a33b911e48c458171464a
snorthway <snorthway@ubuntu.(none)>
2013-03-21 15:14:54 -0400

    fingers crossed for heroku to work

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -1,6 +1,7 @@
 # Django settings for personal_site project.
 
 import os
+import dj_database_url
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,16 +12,18 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'stephn',                      # Or path to database file if using sqlite3.
-        'USER': 'stephn',                      # Not used with sqlite3.
-        'PASSWORD': 't3kant',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+DATABASES['default'] = dj_database_url.config()
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': 'stephn',                      # Or path to database file if using sqlite3.
+#         'USER': 'stephn',                      # Not used with sqlite3.
+#         'PASSWORD': 't3kant',                  # Not used with sqlite3.
+#         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+#         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#     }
+# }
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -163,3 +166,5 @@ LOGGING = {
         },
     }
 }
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
