1496e0c445440b35b46a8fa92e3b13967ee975b7
snorthway <stephanie.northway@students.olin.edu>
2013-08-06 14:17:14 -0400

    email test

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,4 @@
+# Is my email address on this commit?
 from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
